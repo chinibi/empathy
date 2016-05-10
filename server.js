8bbe229743c021c05b98c39457a303b2894eaba4
Trevor Pham <trevor.dt.pham@gmail.com>
2016-05-10 08:24:45 -0700

    about to start cleanup

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(session({
-  secret: 'heyguyshowsitgoin',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }));
