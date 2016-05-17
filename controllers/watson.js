36667c3df952bd7dc070f91c4ee04f92931562c1
Trevor Pham <trevor.dt.pham@gmail.com>
2016-05-17 16:17:50 -0700

    wrote promises as return values of separate functions

--- controllers/watson.js
+++ controllers/watson.js
@@ -21,58 +21,73 @@ var tone_analyzer = watson.tone_analyzer({
   version_date: '2016-02-11'
 });
 
-function analyze(req, res, next) {
-  var query = req.body.daterange.split(" - ")
+function authorize(req, res, next) {
+  if (!req.user) res.redirect("/login")
+  else {
+    res.render('query', { error: null, user: req.user })
+  }
+}
 
-  new Promise(function(resolve, reject) {
+function twitterPromise(username) {
+  return (new Promise(function(resolve, reject) {
     // promise to grab tweets
-    client.get('statuses/user_timeline', {screen_name: req.user.username}, function (err, tweets, resp) {
+    client.get('statuses/user_timeline', {screen_name: username}, function (err, tweets, resp) {
       if (err) reject(err)
       else resolve(resp.body)
     })
-  })
-    .then((tweets) => {
+  }))
+}
+
+function watsonPromise(str) {
+  return (new Promise(function(resolve, reject) {
+    // promise to get analysis from Watson
+    tone_analyzer.tone({text: str}, function(err2, tone) {
+      if (err2) reject(err2)
+      else resolve(tone)
+    })
+  }))
+}
+
+function analyze(req, res, next) {
+  var queryDate = req.body.daterange.split(" - ")
+
+  twitterPromise(req.user.username)
+    .then(tweets => {
       var tweetData = JSON.parse(tweets)
       // filter tweets to date range
-      var wantedData = tweetData.filter((tweet) => {
+      var wantedData = tweetData.filter(tweet => {
         var createdAt = new Date(tweet.created_at)
-        var since = new Date(query[0])
-        var until = new Date(query[1])
+        var since = new Date(queryDate[0])
+        var until = new Date(queryDate[1])
         return (createdAt > since && createdAt < until)
       })
       // concatenate tweets into one string
       var textString = ''
-      wantedData.forEach((tweet) => {
+      wantedData.forEach(tweet => {
         textString += `${tweet.text}. `
       })
       // plug textString into Watson engine
-      return (new Promise(function(resolve, reject) {
-        // promise to get analysis from Watson
-        tone_analyzer.tone({text: textString}, function(err2, tone) {
-          if (err2) reject(err2)
-          else resolve(tone)
-        })
-      }))
-    }, (err) => { // if tweets promise was rejected
+      return watsonPromise(textString)
+    }, (err) => { // if twitterPromise was rejected
       console.log(err)
       res.redirect('/')
     })
-    .then((tone) => {
+    .then(tone => {
       var newReport = new Report({
         user_id: req.user.id,
-        report_name: `${query[0]} to ${query[1]}`,
-        // text: textString,
+        report_name: `${queryDate[0]} to ${queryDate[1]}`,
         created_at: Date.now(),
         tone_categories: tone.document_tone.tone_categories
       })
       newReport.save()
       res.redirect(`/watson/show/${newReport._id}`)
-    }, (err) => { // if watson promise was rejected
+    }, (err) => { // if watsonPromise was rejected
       console.log(err)
       res.render('query', {user: req.user.id, error: err})
     })
 };
 
 module.exports = {
+  authorize: authorize,
   analyze: analyze
 }
